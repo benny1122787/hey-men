b26922cfc71f4f05a48a07f41966d8d87f564dcd
benny <benny1122787@gmail.com>
2023-12-30 00:26:50 +0800

    add ad marquee

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import LandingNavBar from "@/components/landing-navbar"
 import LandingTitleContent from "@/components/landing-title-content"
+import ADMarquee from "@/components/marquee"
 
 export default function LandingPage() {
   return (
     <div>
       <LandingNavBar />
       <LandingTitleContent />
+      <ADMarquee />
     </div>
   )
 }
---
+++ components/marquee.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import Image from "next/image";
+import Link from 'next/link';
+
+const ads = [
+  {
+    label: "VOLVO",
+    src: "/volvo.png",
+    href: "https://www.volvo.com/en/",
+  },
+  {
+    label: "accenture",
+    src: "/accenture.png",
+    href: "https://www.accenture.com/us-en",
+  },
+  {
+    label: "amazon",
+    src: "/amazon.png",
+    href: "https://www.amazon.com/",
+  },
+  {
+    label: "COLUMBIA UNIVERSITY",
+    src: "/ColumbiaUniversity.png",
+    href: "https://www.aljazeera.com/",
+  },
+  {
+    label: "KPMG",
+    src: "/KPMG.png",
+    href: "https://kpmg.com/xx/en/home.html",
+  },
+  {
+    label: "NVIDIA",
+    src: "/nvidia.png",
+    href: "https://www.nvidia.com/en-us/",
+  },
+  {
+    label: "pattern",
+    src: "/pattern.png",
+    href: "https://pattern.com/",
+  },
+  {
+    label: "salesforce",
+    src: "/salesforce.png",
+    href: "https://www.salesforce.com/tw/?ir=1",
+  },
+  {
+    label: "pwc",
+    src: "/pwc.png",
+    href: "https://www.wix.com/",
+  }
+]
+
+export default function ADMarquee() {
+  return (
+    <div>
+      <Marquee autoFill className="h-24" direction="right" speed={50} delay={0} gradient gradientWidth={128}>
+        {ads.map((ad) => (
+          <Link key={ad.label} href={ad.href}>
+            <Image src={ad.src} alt={ad.label} width={80} height={0}/>
+          </Link>
+        ))}
+      </Marquee>
+    </div>
+  )
+}
